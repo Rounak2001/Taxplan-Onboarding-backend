415007b1ac814b675846361231fa8603395ebed2
Advait <advaitsanoj@gmail.com>
2026-02-23 16:52:59 +0530

    add utils

--- backend/utils/supabase_client.py
+++ backend/utils/supabase_client.py
@@ -3,5 +3,7 @@ from django.conf import settings
 
 def get_supabase_client() -> Client:
     url: str = settings.SUPABASE_URL
+    if not url.endswith('/'):
+        url += '/'
     key: str = settings.SUPABASE_KEY
     return create_client(url, key)
